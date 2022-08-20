62823c3568f9b60576c703f5422a71b1fbf90e7e
Nicolas Moreno Alves Medeiros <61037425+NicolasAlvesM@users.noreply.github.com>
2022-08-20 04:13:06 -0300

    sort users by name

---
+++ web/src/utils/sort-helper.js
@@ -0,0 +1,4 @@
+const sortByName = (users) => {
+  users.sort((a,b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : ((b.name.toLowerCase() > a.name.toLowerCase()) ? -1 : 0));
+}
+export default sortByName;
